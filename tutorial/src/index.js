d6614c252eb076610036035a77909908e7b96ec1
Xiaoyu Lyu <lyu.xiaoyu2@mail.dcu.ie>
2022-07-04 15:12:30 +0100

    css

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,21 +1,42 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-function Greeting() {
+//css
+import './index.css'
+
+function Booklist() {
   return (
-    <React.Fragment>
-      <div>
-        <h2>Hello</h2>
-        <Person />
-      </div>
-      <div>
-        <h2>Div prof</h2>
-        <h4>This is Chris, this is my first component</h4>
-      </div>
-    </React.Fragment>
+    <section className='bookList'>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   )
 }
 
-const Person = () => <p>Chris</p>
+const Book = () => {
+  return (
+    <article className='book'>
+      <Image></Image>
+      <Author />
+      <Detail />
+    </article>
+  )
+}
 
-ReactDom.render(<Greeting />, document.getElementById('root'))
+const Image = () => (
+  <img src='https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL302_SR302,200_.jpg' />
+)
+
+const Author = () => <h1>Jenny Han</h1>
+const Detail = () => (
+  <h4 style={{ color: '#617d68', fontsize: '0.75 rem', marginTop: '0.25rem' }}>
+    This book is from azmazon
+  </h4>
+)
+
+ReactDom.render(<Booklist />, document.getElementById('root'))
