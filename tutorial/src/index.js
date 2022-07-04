5df1be45dfddbd4476abae4794de6c721ce7e5dc
Xiaoyu Lyu <lyu.xiaoyu2@mail.dcu.ie>
2022-07-04 14:10:47 +0100

    fundamentals

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,9 +1,21 @@
-import React from "react";
-import ReactDom from "react-dom"
+import React from 'react'
+import ReactDom from 'react-dom'
 
-function Greeting ()
-{
-  return <h4>This is Chris, this is my first component</h4>
+function Greeting() {
+  return (
+    <React.Fragment>
+      <div>
+        <h2>Hello</h2>
+        <Person />
+      </div>
+      <div>
+        <h2>Div prof</h2>
+        <h4>This is Chris, this is my first component</h4>
+      </div>
+    </React.Fragment>
+  )
 }
 
-ReactDom.render(<Greeting/>, document.getElementById("root"));
+const Person = () => <p>Chris</p>
+
+ReactDom.render(<Greeting />, document.getElementById('root'))
