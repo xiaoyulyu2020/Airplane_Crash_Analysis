01bd610430997d8f5792f7e9f171c6b8d87b6c95
Xiaoyu Lyu <lyu.xiaoyu2@mail.dcu.ie>
2022-07-05 14:02:49 +0100

    how to use props in function

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -4,45 +4,37 @@ import ReactDom from 'react-dom'
 //css
 import './index.css'
 
-const firstbook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL302_SR302,200_.jpg',
-  title: 'I Love You to the Moon and Back',
-  author: 'Amelia Hepworth',
-}
-
-const secbook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/91HHxxtA1wL._AC_UL302_SR302,200_.jpg',
-  title: 'The Wonderful Things You Will',
-  author: 'Emilly Winfield Martin',
-}
+const books = [
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL302_SR302,200_.jpg',
+    title: 'I Love You to the Moon and Back',
+    author: 'Amelia Hepworth',
+  },
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/91HHxxtA1wL._AC_UL302_SR302,200_.jpg',
+    title: 'The Wonderful Things You Will',
+    author: 'Emilly Winfield Martin',
+  },
+]
 
 function Booklist() {
   return (
     <section className='bookList'>
-      <Book
-        img={firstbook.img}
-        title={firstbook.title}
-        author={firstbook.author}
-      >
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste
-          doloremque sint nesciunt, iusto sunt voluptatibus itaque excepturi
-          aliquam quidem ipsum?
-        </p>
-      </Book>
-
-      <Book img={secbook.img} title={secbook.title} author={secbook.author} />
+      {books.map((bok) => {
+        const { img, title, author } = bok
+        return <Book books={bok}></Book>
+      })}
     </section>
   )
 }
 
-const Book = ({ img, title, author, children }) => {
+const Book = (props) => {
+  const { img, title, author } = props.books
   return (
     <article className='book'>
       <img src={img} alt='' />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
     </article>
   )
 }
