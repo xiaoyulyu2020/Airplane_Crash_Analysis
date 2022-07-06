c1de18c6464b2bfe0828bf66427c4d98f146f24e
Xiaoyu Lyu <lyu.xiaoyu2@mail.dcu.ie>
2022-07-06 15:29:01 +0100

    onClick onMouseOver and ...

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,3 +1,4 @@
+import { queryAllByAttribute } from '@testing-library/react'
 import React from 'react'
 import ReactDom from 'react-dom'
 
@@ -6,14 +7,22 @@ import './index.css'
 
 const books = [
   {
+    id: 1,
     img: 'https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL302_SR302,200_.jpg',
     title: 'I Love You to the Moon and Back',
     author: 'Amelia Hepworth',
   },
   {
+    id: 2,
     img: 'https://images-na.ssl-images-amazon.com/images/I/91HHxxtA1wL._AC_UL302_SR302,200_.jpg',
     title: 'The Wonderful Things You Will',
     author: 'Emilly Winfield Martin',
+  },
+  {
+    id: 3,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71Mx-bE91+L._AC_UL604_SR604,400_.jpg',
+    title: "It's Not Summer Without You",
+    author: 'Jenny Han',
   },
 ]
 
@@ -21,20 +30,33 @@ function Booklist() {
   return (
     <section className='bookList'>
       {books.map((bok) => {
-        const { img, title, author } = bok
-        return <Book books={bok}></Book>
+        return <Book key={bok.id} {...bok}></Book>
       })}
     </section>
   )
 }
+const clickHandler = (event) => {
+  console.log(event.target)
+  alert('Hellowwww')
+}
+const moreDetail = (author) => {
+  console.log(author)
+}
 
-const Book = (props) => {
-  const { img, title, author } = props.books
+function Book(props) {
+  const { img, title, author } = props
   return (
-    <article className='book'>
+    <article className='book' onMouseOver={() => {}}>
       <img src={img} alt='' />
-      <h1>{title}</h1>
+      <h1 onClick={() => alert("Don't touch me!")}>{title}</h1>
       <h4>{author}</h4>
+      <button id='myButton' type='button' onClick={clickHandler}>
+        click
+      </button>
+      <br></br>
+      <button type='button' onClick={() => moreDetail(author)}>
+        For detail click me!
+      </button>
     </article>
   )
 }
